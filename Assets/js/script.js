9fc34f552839cf8e5613ac20be6696f522a37f5b
priscilla ma <priscillama621@gmail.com>
2022-02-10 00:43:40 -0500

    switched out construction html with regular site, updated site- reformatted the style, removed selfie, added recent projects and work

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,3 @@
 // linkedin
-// Have a link to a downloadable PDF of your resume
-// Project title, Link to deploy, Link to GitHub repo, Screenshot of application
-
+// Have a link to a downloadable PDF of your resume🤎
+// Project title, Link to deploy, Link to GitHub repo, Screenshot of application🤎
